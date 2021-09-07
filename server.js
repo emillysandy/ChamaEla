87b38c795b97eb10cc53942902b6f9cf4e9333b5
Emilly Sandy <emillysandy05@gmail.com>
2021-09-07 15:23:57 -0300

    Atualizando porta

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require("./src/app.js")
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT,() =>{
     console.log(`Meu servidor rodando na porta ${PORT}`) 
